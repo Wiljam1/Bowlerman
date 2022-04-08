eeb88ddfa8cab07fdbaa7ed08944653a49329e8a
Wiljam1 <44192121+Wiljam1@users.noreply.github.com>
2022-04-08 11:12:33 +0200

    Created template

---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Renderer* renderer;
+    SDL_Window *window = SDL_CreateWindow("Hello SDL WORLD", 
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
+    SDL_WINDOW_ALLOW_HIGHDPI);
+    // We must call SDL_CreateRenderer in order for draw calls to affect this window.
+        renderer = SDL_CreateRenderer(window, -1, 0);
+
+        // Select the color for drawing. It is set to red here.
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+
+        // Clear the entire screen to our selected color.
+        SDL_RenderClear(renderer);
+
+        // Up until now everything was drawn behind the scenes.
+        // This will show the new, red contents of the window.
+        SDL_RenderPresent(renderer);
+    
+    if(NULL == window)
+    {
+        return 1;
+    }
+    SDL_Event windowEvent;
+    while(1)
+    {
+        if(SDL_PollEvent(&windowEvent))
+        {
+            if(SDL_QUIT == windowEvent.type)
+            {break;}
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
