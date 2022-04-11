ecf1248136c932e9ea9be0bea83f61f14d5ab852
Emil Wennström <emilwennstrom@gmail.com>
2022-04-11 10:56:57 +0200

    Lite mellanslag

--- src/main.c
+++ src/main.c
@@ -6,6 +6,5 @@ int main()
     Application theApp = createApplication();
     applicationUpdate(theApp);
     destoryApplication(theApp);
-    printf("Niklas har strul med SDL_net\n");
     return 0;
 }
