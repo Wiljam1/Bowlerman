c0b5a3cdd53f657763f236062fbde9dabe7855b3
Niklas Roslund <nroslund@kth.se>
2022-04-24 09:47:51 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Wiljam1/Grupp2""
    This reverts commit db6fce3dfe2952fa8e82ed4d4a7ffbb22c77843d.

--- Bowlerman/player.c
+++ Bowlerman/player.c
@@ -1,27 +1,28 @@
-#include "player.h"
-#include "game.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-
+#include "player.h"
+#include "game.h"
+#include "collissionDetection.h"
 
 #define PUBLIC /* empty */
 #define PRIVATE static
 
-PRIVATE int HEIGHT = 300;
-PRIVATE int WIDTH = 150;
+PRIVATE int PLAYERHEIGHT = 100;
+PRIVATE int PLAYERWIDTH = 50;
+PRIVATE int PLAYERSPEED = 300;     // speed in pixels/second
+
 
 PUBLIC void loadPlayer();
 PUBLIC Player initPlayer(float xPos, float yPos);
 PUBLIC void initPlayerRect(SDL_Rect *playerRect, Player player1);
 
-struct playerController
-{
-    float xPos;
-    float yPos;
-    float speed;
-    //SDL_Rect playerRectangle;   //struct to hold the position and size of the sprite
-};
+
+
+
+
 
 PUBLIC SDL_Rect playerRect[4];
 
@@ -30,7 +31,7 @@ PUBLIC void initPlayerRect(SDL_Rect * playerRect, Player player1)
     playerRect->h = getPlayerHeight();
     playerRect->w = getPlayerWidth();
     playerRect->y = getPlayerYPosition(player1);
-    playerRect->x = getPlayerYPosition(player1);
+    playerRect->x = getPlayerXPosition(player1);
 }
 
 
@@ -38,8 +39,14 @@ PUBLIC Player initPlayer(float xPos, float yPos)
 {
     Player p = malloc(sizeof(struct playerController));
 
-    p->xPos = xPos;
-    p->yPos = yPos;
+    p->up=0;
+    p->down=0;
+    p->right=0;
+    p->left=0;
+    p->xVel=0;
+    p->yVel=0;
+    p->xPos = p->xPosOld = xPos;
+    p->yPos = p->yPosOld = yPos;
     p->speed = 1;
 
     return p;
@@ -48,11 +55,11 @@ PUBLIC Player initPlayer(float xPos, float yPos)
 
 PUBLIC int getPlayerHeight()
 {
-    return HEIGHT;
+    return PLAYERHEIGHT;
 }
 PUBLIC int getPlayerWidth()
 {
-    return WIDTH;
+    return PLAYERWIDTH;
 }
 PUBLIC float getPlayerXPosition(Player player)
 {
@@ -61,6 +68,21 @@ PUBLIC float getPlayerXPosition(Player player)
 PUBLIC float getPlayerYPosition(Player player)
 {
     return player->yPos;
+}
+
+PUBLIC void determinePlayerVelocity(Player playerMoving)
+{
+    playerMoving->xVel=0;
+    playerMoving->yVel=0;
+    if(playerMoving->up && !playerMoving->down) playerMoving->yVel = -PLAYERSPEED;
+    if(playerMoving->down && !playerMoving->up) playerMoving->yVel = PLAYERSPEED;
+    if(playerMoving->left && !playerMoving->right) playerMoving->xVel = -PLAYERSPEED;
+    if(playerMoving->right && !playerMoving->left) playerMoving->xVel = PLAYERSPEED;
+}
+PUBLIC void updatePlayerClientPosition(Player playerMoving)
+{
+    playerMoving->xPos+= playerMoving->xVel / 60;
+    playerMoving->yPos+= playerMoving->yVel / 60;
 }
 
 // PUBLIC void destroyPlayer(Player player)
