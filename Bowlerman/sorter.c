57fbab5097f4b3f40368dbb480f2c8d3563cdbfe
Wiljam1 <44192121+Wiljam1@users.noreply.github.com>
2022-04-25 16:15:12 +0200

    Revert "FLyttade game struct till game.h"
    This reverts commit b6e3ce0ae4743be3ac5ef7e1a36703f8e61ae93e.

--- Bowlerman/sorter.c
+++ Bowlerman/sorter.c
@@ -1,40 +1,42 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <stdbool.h>
- #include <stdbool.h>
- #include <SDL2/SDL.h>
- #include <SDL2/SDL_image.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdbool.h>
+// #include <stdbool.h>
+// #include <SDL2/SDL.h>
+// #include <SDL2/SDL_image.h>
+// #include "player.h"
+// #include "game.h"
+// #include "collissionDetection.h"
+// #include "sorter.h"
 
- #include "game.h"
- #include "player.h"
- #include "sorter.h"
- //bubble sorter:
- void arraySorter(Game theGame, int length, int *v)
- {
-     //reset array
-     int b[10];
-     for(int i=0; i<length;  i++)
-     {
-        b[i]=theGame->player[i].yPos;
-        v[i]=i;
-     }
-     //sort array
-     int j,tmp;
-     for(int i=0;i<length -1;i++)
-     {
-         for(j=0;j<length -1-i;j++)
-         {
-             if(b[j] > b[j+1])
-             {
-                 tmp=b[j];
-                 b[j]=b[j+1];
-                 b[j+1]=tmp;
-              
-                 tmp=v[j];
-                 v[j]=v[j+1];
-                 v[j+1]=tmp;
-             }
-         }
-     }
- } 
+// //bubble sorter:
+// void arraySorter(Player *player, int length, int *v)
+// {
+//     //reset array
+//     int b[length];
+//     for(int i=0; i<length;  i++)
+//     {
+//         b[i]=player[i]->yPos;
+//         v[i]=i;
+//     }
+
+//     //sort array
+//     int j,tmp;
+//     for(int i=0;i<length -1;i++)
+//     {
+//         for(j=0;j<length -1-i;j++)
+//         {
+//             if(b[j] > b[j+1])
+//             {
+//                 tmp=b[j];
+//                 b[j]=b[j+1];
+//                 b[j+1]=tmp;
+                
+//                 tmp=v[j];
+//                 v[j]=v[j+1];
+//                 v[j+1]=tmp;
+//             }
+//         }
+//     }
+// } 
 
