5650b78854f2a12576a52a2348006f1f048474ab
Wiljam Wilmi <wiljam@kth.se>
2022-05-02 18:20:49 +0200

    Update start.bat & lite cleanup & kommentarer
    Nu öppnar och stänger start.bat servern också (och en klient).

--- Bowlerman/start.bat
+++ Bowlerman/start.bat
@@ -1,3 +1,9 @@
-make windows
-main.exe
 make clean_windows
+make UDPserver
+make windows
+start UDPserver.exe        
+:: ^START GÖR ATT DEN GÅR VIDARE TILL NÄSTA "RAD" MEDAN BATCH-FILEN KÖRS
+main.exe                   
+:: ^INGEN START BETYDER ATT DEN VÄNTAR MED NÄSTA RAD TILLS MAN STÄNGT NER .EXE FILEN
+taskkill /im UDPserver.exe 
+:: ^STÄNGER NER SERVERN EFTER MAN STÄNGT SIN CLIENT
