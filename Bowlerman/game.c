8620c049c26dbeea842554c38ded3a700da9beb4
Niklas Roslund <nroslund@kth.se>
2022-05-03 11:11:09 +0200

    nytt försök

--- Bowlerman/game.c
+++ Bowlerman/game.c
@@ -164,7 +164,7 @@ void initGame(Game theGame)
         for(int j=0;j<7;j++)
         {
             theGame->wall[i*j+WALLAMOUNT] = initWalls(WALLAMOUNT*2, wallwidth,wallheight);
-            theGame->wall[i*j+WALLAMOUNT] = wallPlace(i*j*70+140, i*j*70+140);
+            theGame->wall[i*j+WALLAMOUNT] = wallPlace(j*70+140, i*70+140);
         }
     }
 }
@@ -583,14 +583,10 @@ PRIVATE void renderWalls(Game theGame)
             SDL_RenderCopyEx(theGame->renderer, theGame->textureWall[1], NULL, &wallRect, 270, 0, 0);
         }
     }
-    for (int i=1;i<5;i++)
-    {
-        for(int j=0;j<7;j++)
-        {
-            SDL_Rect wallRect = {theGame->wall[i*j+WALLAMOUNT].x, theGame->wall[i*j+WALLAMOUNT].y, theGame->wall[i*j+WALLAMOUNT].w, theGame->wall[i*j+WALLAMOUNT].h};
-            SDL_RenderCopy(theGame->renderer, theGame->textureWall[2], NULL, &wallRect);
-            
-        }
+    for (int i=0;i<28;i++)
+    {   
+        SDL_Rect wallRect = {theGame->wall[i+WALLAMOUNT].x, theGame->wall[i+WALLAMOUNT].y, theGame->wall[i+WALLAMOUNT].w, theGame->wall[i+WALLAMOUNT].h};
+        SDL_RenderCopy(theGame->renderer, theGame->textureWall[2], NULL, &wallRect);        
     }
 
 
