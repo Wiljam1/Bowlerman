c9ac5de7aa938106cffdc3763573480b75a54f75
Emil Wennström <emilwennstrom@gmail.com>
2022-05-05 20:32:22 +0200

    Update game.c

--- Bowlerman/game.c
+++ Bowlerman/game.c
@@ -533,47 +533,6 @@ void renderTextures(Game theGame)
     SDL_RenderPresent(renderer); // present renderer
 }
 
-PRIVATE void loadAllTextures(Game theGame)
-{
-    // loads in textures
-    theGame->background = (SDL_Texture *)loadTextures(theGame, "alley.png");
-    /*PLAYER ID 0*/
-    theGame->player_texture[0][0] = (SDL_Texture *)loadTextures(theGame, "blueman/down.png");
-    theGame->player_texture[0][1] = (SDL_Texture *)loadTextures(theGame, "blueman/up.png");
-    theGame->player_texture[0][2] = (SDL_Texture *)loadTextures(theGame, "blueman/right.png");
-    theGame->player_texture[0][3] = (SDL_Texture *)loadTextures(theGame, "blueman/left.png");
-    /*PLAYER ID 1*/
-    theGame->player_texture[1][0] = (SDL_Texture *)loadTextures(theGame, "purpleman/down.png");
-    theGame->player_texture[1][1] = (SDL_Texture *)loadTextures(theGame, "purpleman/up.png");
-    theGame->player_texture[1][2] = (SDL_Texture *)loadTextures(theGame, "purpleman/right.png");
-    theGame->player_texture[1][3] = (SDL_Texture *)loadTextures(theGame, "purpleman/left.png");
-    /*PLAYER ID 2*/
-    theGame->player_texture[2][0] = (SDL_Texture *)loadTextures(theGame, "redman/down.png");
-    theGame->player_texture[2][1] = (SDL_Texture *)loadTextures(theGame, "redman/up.png");
-    theGame->player_texture[2][2] = (SDL_Texture *)loadTextures(theGame, "redman/right.png");
-    theGame->player_texture[2][3] = (SDL_Texture *)loadTextures(theGame, "redman/left.png");
-    /*PLAYER ID 3*/
-    theGame->player_texture[3][0] = (SDL_Texture *)loadTextures(theGame, "redman/down.png");
-    theGame->player_texture[3][1] = (SDL_Texture *)loadTextures(theGame, "redman/up.png");
-    theGame->player_texture[3][2] = (SDL_Texture *)loadTextures(theGame, "redman/right.png");
-    theGame->player_texture[3][3] = (SDL_Texture *)loadTextures(theGame, "redman/left.png");
-    /*BOWLING BALLS*/
-    theGame->bomb_texture[0] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_BLue.png");
-    theGame->bomb_texture[1] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Purple.png");
-    theGame->bomb_texture[2] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Red.png");
-    theGame->bomb_texture[3] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Yellow.png");
-    theGame->bombExplosion_texture = (SDL_Texture *)loadTextures(theGame, "FIRE.png");
-    /*WALLS*/
-    theGame->textureWall[0] = (SDL_Texture *)loadTextures(theGame, "walls/80/straight.png");
-    theGame->textureWall[1] = (SDL_Texture *)loadTextures(theGame, "walls/80/corner.png");
-    theGame->textureWall[2] = (SDL_Texture *)loadTextures(theGame, "walls/80/center.png");
-    theGame->textureWall[3] = (SDL_Texture *)loadTextures(theGame, "wall.png");
-    SDL_FreeSurface(theGame->window_surface);
-    // Load player sprites
-    theGame->pSprites = GetPlayerSprite();
-    // Load bomb sprites
-    loadBomb();
-}
 
 PRIVATE void UpdatePlayerTextures(Game theGame)
 {
@@ -744,4 +703,46 @@ void initExplosionPosition(Game theGame, int playerID)
             }
         }
     }
+}
+
+PRIVATE void loadAllTextures(Game theGame)
+{
+    // loads in textures
+    theGame->background = (SDL_Texture *)loadTextures(theGame, "alley.png");
+    /*PLAYER ID 0*/
+    theGame->player_texture[0][0] = (SDL_Texture *)loadTextures(theGame, "blueman/down.png");
+    theGame->player_texture[0][1] = (SDL_Texture *)loadTextures(theGame, "blueman/up.png");
+    theGame->player_texture[0][2] = (SDL_Texture *)loadTextures(theGame, "blueman/right.png");
+    theGame->player_texture[0][3] = (SDL_Texture *)loadTextures(theGame, "blueman/left.png");
+    /*PLAYER ID 1*/
+    theGame->player_texture[1][0] = (SDL_Texture *)loadTextures(theGame, "purpleman/down.png");
+    theGame->player_texture[1][1] = (SDL_Texture *)loadTextures(theGame, "purpleman/up.png");
+    theGame->player_texture[1][2] = (SDL_Texture *)loadTextures(theGame, "purpleman/right.png");
+    theGame->player_texture[1][3] = (SDL_Texture *)loadTextures(theGame, "purpleman/left.png");
+    /*PLAYER ID 2*/
+    theGame->player_texture[2][0] = (SDL_Texture *)loadTextures(theGame, "redman/down.png");
+    theGame->player_texture[2][1] = (SDL_Texture *)loadTextures(theGame, "redman/up.png");
+    theGame->player_texture[2][2] = (SDL_Texture *)loadTextures(theGame, "redman/right.png");
+    theGame->player_texture[2][3] = (SDL_Texture *)loadTextures(theGame, "redman/left.png");
+    /*PLAYER ID 3*/
+    theGame->player_texture[3][0] = (SDL_Texture *)loadTextures(theGame, "redman/down.png");
+    theGame->player_texture[3][1] = (SDL_Texture *)loadTextures(theGame, "redman/up.png");
+    theGame->player_texture[3][2] = (SDL_Texture *)loadTextures(theGame, "redman/right.png");
+    theGame->player_texture[3][3] = (SDL_Texture *)loadTextures(theGame, "redman/left.png");
+    /*BOWLING BALLS*/
+    theGame->bomb_texture[0] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_BLue.png");
+    theGame->bomb_texture[1] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Purple.png");
+    theGame->bomb_texture[2] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Red.png");
+    theGame->bomb_texture[3] = (SDL_Texture *)loadTextures(theGame, "Bowling_Ball_Yellow.png");
+    theGame->bombExplosion_texture = (SDL_Texture *)loadTextures(theGame, "FIRE.png");
+    /*WALLS*/
+    theGame->textureWall[0] = (SDL_Texture *)loadTextures(theGame, "walls/80/straight.png");
+    theGame->textureWall[1] = (SDL_Texture *)loadTextures(theGame, "walls/80/corner.png");
+    theGame->textureWall[2] = (SDL_Texture *)loadTextures(theGame, "walls/80/center.png");
+    theGame->textureWall[3] = (SDL_Texture *)loadTextures(theGame, "wall.png");
+    SDL_FreeSurface(theGame->window_surface);
+    // Load player sprites
+    theGame->pSprites = GetPlayerSprite();
+    // Load bomb sprites
+    loadBomb();
 }
