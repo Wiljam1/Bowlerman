6eb470b540276c568bb8306e46b0a47bd3d53246
Wiljam1 <44192121+Wiljam1@users.noreply.github.com>
2022-05-05 17:31:15 +0200

    Cleanup

--- Bowlerman/bomb.c
+++ Bowlerman/bomb.c
@@ -100,8 +100,9 @@ void process(Game theGame, Sounds *s)
                 if(theGame->wall[j].destroyedWall == 0){
                     theGame->wall[j].destroyedWall = testCollisionWithDestroyableWalls(theGame, j, i);
                     if(theGame->wall[j].destroyedWall){ //If wall is destroyed...
-                        if(theGame->playerIDLocal % j == 1){
+                        if(1){
                             printf("Own Explosion! on client %d\n", theGame->playerIDLocal);
+                            printf("playerID: %d\n", theGame->playerIDLocal);
                             rollForPowerup(theGame, theGame->wall[j].x, theGame->wall[j].y);        //kallar på powerupp
                         }
                     }
@@ -125,7 +126,7 @@ void sortBombsArray(Game theGame,int i)
 {
     Bowlingball tmp;
     int temporarytime=0;
-    //for (int k=0;k<theGame->player[i].amountOfBombsPlaced;k++)
+    for (int k=0;k<theGame->player[i].amountOfBombsPlaced;k++)
     {
         for (int j = 1;j<theGame->player[i].amountOfBombsPlaced;j++)
         {
