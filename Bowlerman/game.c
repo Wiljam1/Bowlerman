c1133e47778a5e22e1fe4e30ab558c06dd6eadf1
Wiljam1 <44192121+Wiljam1@users.noreply.github.com>
2022-05-12 14:03:11 +0200

    Update game.c

--- Bowlerman/game.c
+++ Bowlerman/game.c
@@ -193,7 +193,7 @@ PUBLIC void gameUpdate(Game theGame)
         // render display
         renderTextures(theGame);
 
-        SDL_Delay(1000 / 60); // man behöver ta minus här för att räkna in hur lång tid spelet tar att exekvera
+        SDL_Delay(1); // man behöver ta minus här för att räkna in hur lång tid spelet tar att exekvera
     }
     //destroySoundFiles(sounds);
 }
